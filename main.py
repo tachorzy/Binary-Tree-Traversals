7cf2a0663cafb31427eecdb2f7b8ec11f7fde815
Tariq A <tachorzyad@gmail.com>
2022-11-06 16:11:21 -0600

    final solution

--- main.py
+++ main.py
@@ -1,46 +1,26 @@
-#     def printInorder(self):
-#         if self.left():
-#             self.left.printInorder()
-#         print(self.num)
-#         if self.right():
-#             self.right.printInorder()
-
-#     def printPostorder(self):
-#         if self.left():
-#             self.left.printPostorder()
-#         if self.right():
-#             self.right.printPostorder()
-#         print(self.num)
-
-#     def printPreorder(self):
-#         print(self.num)
-#         if self.left():
-#             self.left.printPreorder()
-#         if self.right():
-#             self.right.printPreorder()
-
 #function searches for the root in the given inorder traversal, all values to its right will be right nodes
 def search(inorder, root):
     for i in range(len(inorder)):
         if(inorder[i] == root):
             return i
 
-def postorder(n, preorder, inorder):
+def postorder(n: int, preorder: list, inorder: list):
     root = search(inorder, preorder[0])
     
     if(root != 0): 
-        postorder(root, preorder + 1, inorder)
+        postorder(root, preorder[1:], inorder)
     
     if(root != n-1):
-        postorder(n - root - 1, preorder + root + 1, inorder + root + 1)
-
-    return
-
+        postorder(n - root - 1, preorder[root+1:], inorder[root+1:])
+    
+    print(preorder[0], end=" ")
 
 def main():
     n = int(input())
     preorder = [int(x) for x in input().split()]
     inorder = [int(x) for x in input().split()]
 
+    postorder(n, preorder, inorder)
+    
 if __name__ == "__main__":
-    main()  
+  main()
